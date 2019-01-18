8d38d608b1350b5b1886dad144e0f6f2c25133af
Valerian Perez <valerian.perez@gmail.com>
2019-01-18 20:42:16 +0100

    remove cmake file

--- cmake/SUBMODULE.cmake
+++
@@ -1,9 +0,0 @@
-include(ExternalProject)
-
-
-set(SUBMODULE_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/submodule)
-# as CMAKE_INSTALL_PREFIX is set to ROGUE_BUILD_DIRECTORY by default, we might also use it
-set(SUBMODULE_INSTALL_DIRECTORY ${BUILD_DIRECTORY}/submodule)
-set(SUBMODULE_BUILD_TYPE Release)
-
-include(SUBGLM)
