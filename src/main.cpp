90215dcf141fba756aef276936c11137292deced
Valerian Perez <valerian.perez@gmail.com>
2019-01-18 18:52:55 +0100

    merge in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include "ImageManager/imagemanager.hpp"
+#include "CellTree/CellTree.hpp"
 
 int main(int, char *[]) {
     //Initialization of Image Manager
@@ -9,6 +10,15 @@ int main(int, char *[]) {
     img = im->imread("de.png");
 
     std::vector<glm::vec2> cloud = im->getCoord(img);
+
+/*//suppression
+    cv::Size sz = img.size();
+    CellTree tree = CellTree(0,300,0,300);
+    tree.subdivideCell();
+
+    tree.displayMe();
+
+//*/
 
     std::cout << "is ok, press something" << std::endl;       
     return 0;
